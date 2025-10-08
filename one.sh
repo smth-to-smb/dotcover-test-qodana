9e2fe2634a8d9feb96465726288156e7c0a07dfe
Anton Samarin <48833312+smth-to-smb@users.noreply.github.com>
2025-10-08 13:27:23 +0200

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,5 +1,5 @@
 docker run \
 -v $(pwd)/:/data/project/ \
 -v .qodana/code-coverage/:/data/coverage \
--e QODANA_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0IjoiTlZPVkciLCJvcmdhbml6YXRpb24iOiJYNlFXMCIsInRva2VuIjoiajQ2N2sifQ.68_RoXmAcptVY9T9VDuv_5H9x2vgyM18fVPwQHWP77U" \
+-e QODANA_TOKEN="" \
 94370f01ba4a
